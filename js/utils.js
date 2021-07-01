9a00503a09920a14e36b646f642018c4a29f35a1
Omri Shalev <omriknight9@gmail.com>
2021-07-01 18:22:18 +0300

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+const hasClass = (elem, className) => {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+const addClass = (elem, className) => {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+
+const toggleClass = (elem, className) => {
+    let newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
