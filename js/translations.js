1d637be4dfde37b92176c68c5622aa97abaaf608
Omri Shalev <omriknight9@gmail.com>
2021-07-01 18:22:07 +0300

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -1,84 +1,76 @@
-function changeToHeb() {
+
+const changeToHeb = (type) => {
+    lang = 2;
     $('#birthdayToday').empty();
     $('.closestBirth, .birthdayWish').remove();
-
-    lang = 2;
-    $('#langBtnHe').css('pointer-events', 'none');
-    $('#langBtnEn').css('pointer-events', 'all');
-
+    $('#langBtnHe').css({'pointer-events': 'none', 'opacity': '.7'});
+    $('#langBtnEn').css({'pointer-events': 'all', 'opacity': '1'});
+    $('#langBtnHe span').html('עברית');
+    $('#langBtnEn span').html('אנגלית');
     $("head").append("<link rel='stylesheet' type='text/css' href='css/main_he.css' id='hebCss'/>");
     $('title').html('ימי הולדת - משפחה');
-
     $('h1').hide().html('ימי הולדת - משפחה').fadeIn('slow');
     $('#search').hide().attr('placeholder', 'חפש במשפחה').fadeIn('slow');
-
     $('#closestBirth').html('');
     $('#BirthdayWish').html('');
-
     $('.sortBtn').hide().html('מיין').fadeIn('slow');
-    $('.shalevsBtn').hide().html('משפחת שלו').fadeIn('slow');
-    $('.waizingersBtn').hide().html('משפחת ויזינגר').fadeIn('slow');
-    $('.alayevsBtn').hide().html('משפחת אלייב').fadeIn('slow');
-
     $('.ageSortBtn').hide().html('לפי גיל').fadeIn('slow');
     $('.nameSortBtn').hide().html('לפי שם').fadeIn('slow');
     $('.groupSortBtn').hide().html('לפי קבוצה').fadeIn('slow');
     $('.calendarSortBtn').hide().html('לפי תאריך').fadeIn('slow');
-
     $('#checkBirthdaysLink').hide().html('בדוק באיזה יום נופל היומולדת בשנים הקרובות').fadeIn('slow');
-
     $('.hebCaneldar').show();
     $('.engCaneldar').hide();
-
     $('.popupBtn').hide().html('סגור').fadeIn('slow');
+    $('#langMenuHeader').html('שפה');
+    $('#familyMenuHeader').html('משפחה');
+    $('#shalevsLi span').html('משפחת שלו');
+    $('#waizingersLi span').html('משפחת ויזינגר');
+    $('#alayevsLi span').html('משפחת אלייב');
 
-
-    switch (familyNum) {
-        case 1:
-            showFamily(1);
-            break;
-        case 2:
-            showFamily(2);
-            break;
-        case 3:
-            showFamily(3);
-            break;
+    if (type == 1) {
+        switch (familyNum) {
+            case 1:
+                showFamily(1);
+                break;
+            case 2:
+                showFamily(2);
+                break;
+            case 3:
+                showFamily(3);
+                break;
+        }
     }
 }
 
-function changeToEng() {
+const changeToEng = () => {
+    lang = 1;
     $('#birthdayToday').empty();
     $('.closestBirth, .birthdayWish').remove();
-    
-    lang = 1;
-    $('#langBtnEn').css('pointer-events', 'none');
-    $('#langBtnHe').css('pointer-events', 'all');
-
+    $('#langBtnEn').css({'pointer-events': 'none', 'opacity': '.7'});
+    $('#langBtnHe').css({'pointer-events': 'all', 'opacity': '1'});
+    $('#langBtnHe span').html('Hebrew');
+    $('#langBtnEn span').html('English');
     $('#hebCss').remove();
     $('title').html('Family Birthdays');
     $('h1').hide().html('Family Birthdays').fadeIn('slow');
     $('#search').hide().attr('placeholder', 'Search A Family Member').fadeIn('slow');
-
     $('#closestBirth').html('');
     $('#BirthdayWish').html('');
-
     $('.sortBtn').hide().html('Sort').fadeIn('slow');
-    $('.shalevsBtn').hide().html('Shalevs').fadeIn('slow');
-    $('.waizingersBtn').hide().html('Waizingers').fadeIn('slow');
-    $('.alayevsBtn').hide().html('Alayevs').fadeIn('slow');
-
     $('.ageSortBtn').hide().html('By Age').fadeIn('slow');
     $('.nameSortBtn').hide().html('By Name').fadeIn('slow');
     $('.groupSortBtn').hide().html('By Group').fadeIn('slow');
     $('.calendarSortBtn').hide().html('By Date').fadeIn('slow');
-
     $('#checkBirthdaysLink').hide().html('Check What Day Is The Birthday For Upcoming Years').fadeIn('slow');
-
     $('.hebCaneldar').hide();
     $('.engCaneldar').show();
-
     $('.popupBtn').hide().html('Close').fadeIn('slow');
-
+    $('#langMenuHeader').html('Lang');
+    $('#familyMenuHeader').html('Family');
+    $('#shalevsLi span').html('Shalevs');
+    $('#waizingersLi span').html('Waizingers');
+    $('#alayevsLi span').html('Alayevs');
     switch (familyNum) {
         case 1:
             showFamily(1);
